7206b415721d901f97a54ba73a8297d56f6c08ab
Luis <luis@memorelab.com>
2019-02-10 10:54:51 -0200

    em algo gaussian mixture

--- em/emalgo.go
+++ em/emalgo.go
@@ -3,8 +3,15 @@ package em
 import (
 	"fmt"
 	"golang.org/x/exp/rand"
+	"gonum.org/v1/gonum/floats"
 	"gonum.org/v1/gonum/mat"
+	"gonum.org/v1/gonum/stat"
 	"gonum.org/v1/gonum/stat/distmv"
+	"gonum.org/v1/gonum/stat/distuv"
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
+	"math"
 )
 
 
@@ -22,7 +29,127 @@ func GenerateMVGaussian(means []float64, sigma *mat.DiagDense, numObs int, seed 
 	return data
 }
 
+func PlotDataset(fileName string, dataset []float64){
+	v := make(plotter.Values, len(dataset))
+	for i, elem := range dataset{
+		v[i] = elem
+	}
 
-func EMAlgo(){
+	p, err := plot.New()
+	if err != nil {
+		panic(err)
+	}
+	h, err := plotter.NewHist(v, 16)
+	if err != nil {
+		panic(err)
+	}
+	p.Add(h)
+	if err := p.Save(4*vg.Inch, 4*vg.Inch, fileName); err != nil {
+		panic(err)
+	}
+}
+
+
+func GenerateMixtureGaussians(responsability float64, gaussianData [][]float64) []float64{
+	if responsability < 0 || responsability > 1 {
+		panic("Responsability must be between 0 and 1!")
+	}
+	var ber = distuv.Bernoulli{P:responsability}
+
+	var numObs = len(gaussianData[0])
+	var finalData = make([]float64, numObs)
+	for vec:=0; vec< numObs; vec ++{
+		tmpDraw := ber.Rand()
+		if tmpDraw == 1{
+			finalData[vec] = gaussianData[0][vec]
+		}else{
+			finalData[vec] = gaussianData[1][vec]
+		}
+	}
+	return finalData
+}
+
+
+func EMAlgo(dataset []float64, tol int) ([]float64, float64, float64,float64, float64,float64){
+
+	// Initialize parameters
+	sigmaZero := stat.Variance(dataset, nil)
+	muZero := dataset[rand.Intn(len(dataset))]
+	sigmaOne := sigmaZero
+	muOne := dataset[rand.Intn(len(dataset))]
+	piHat := rand.Float64()
+	round := 0
+
+	fmt.Printf("===================== \n")
+	fmt.Printf("INITIALIZATION: \n")
+	fmt.Printf("mu_zero: %v \n", muZero)
+	fmt.Printf("mu_one: %v \n", muOne)
+	fmt.Printf("sigma_zero: %v \n", sigmaZero)
+	fmt.Printf("sigma_one: %v \n", sigmaOne)
+	fmt.Printf("pi_hat: %v \n", piHat)
+
+	// Auxiliary variables to the optimization step
+	sigmaZeroRep := make([]float64, len(dataset))
+	sigmaOneRep := make([]float64, len(dataset))
+	muZeroRep := make([]float64, len(dataset))
+	muOneRep := make([]float64, len(dataset))
+	gammaRep :=  make([]float64, len(dataset))
+	gamma := make([]float64, len(dataset))
+	likelihoods := make([]float64, tol)
+
+	for ; round < tol;{
+
+		distOne := distuv.Normal{
+			Mu:    muOne,
+			Sigma: sigmaOne,
+		}
+
+		distZero := distuv.Normal{
+			Mu:    muZero,
+			Sigma: sigmaZero,
+		}
+
+		// Expectation Step - Compute Responsabilities
+		for j:=0; j< len(dataset); j++{
+			gamma[j] = (piHat*distZero.Prob(dataset[j]))/((1.0-piHat)*distOne.Prob(dataset[j]) + piHat*distZero.Prob(dataset[j]))
+		}
+
+		// Maximization Step
+		for i:=0;i<len(gamma);i++{
+			gammaRep[i] = 1.0 - gamma[i]
+			muZeroRep[i] = gamma[i]*dataset[i]
+			muOneRep[i] = (1.0 - gamma[i])*dataset[i]
+		}
+
+		gammaSum := floats.Sum(gamma)
+		gammaRepSum := floats.Sum(gammaRep)
+
+		// Update estimators
+		muZero = floats.Sum(muZeroRep)/ gammaSum
+		muOne = floats.Sum(muOneRep)/ gammaRepSum
+
+		for i:=0;i<len(gamma);i++{
+			sigmaOneRep[i] = (1.0 - gamma[i])*math.Pow(dataset[i] - muOne, 2)
+			sigmaZeroRep[i] = gamma[i]*math.Pow(dataset[i] - muZero, 2)
+		}
+
+		sigmaZero = floats.Sum(sigmaZeroRep) / gammaSum
+		sigmaOne = floats.Sum(sigmaOneRep) / gammaRepSum
+		piHat = gammaSum / float64(len(gamma))
+
+		likelihoods[round] = logLikelihood(piHat, dataset, distZero, distOne)
+		round++
+	}
+
+	return likelihoods, muZero, muOne, sigmaZero, sigmaOne, piHat
 
 }
+
+func logLikelihood(pi float64, dataset []float64, normalZero distuv.Normal, normalOne distuv.Normal) float64{
+	tmpRep := make([]float64, len(dataset))
+	for i:= range tmpRep{
+		tmpRep[i] = math.Log((1.0-pi)*normalOne.Prob(dataset[i]) + pi*normalZero.Prob(dataset[i]))
+	}
+	return floats.Sum(tmpRep)
+}
+
--- em/emalgo_test.go
+++ em/emalgo_test.go
@@ -3,6 +3,9 @@ package em
 import (
 	"fmt"
 	"gonum.org/v1/gonum/mat"
+	"gonum.org/v1/gonum/stat/distuv"
+	"math/rand"
+
 	"testing"
 )
 
@@ -17,4 +20,46 @@ func TestGenerateMVGaussian(t *testing.T){
 	for _, vec := range dataSet{
 		fmt.Printf("%v \n", vec)
 	}
+}
+
+func TestEMAlgo(t *testing.T) {
+	means := []int{-3,3}
+	numObs := 1000
+	sigma := 1.5
+	responsability := 0.7
+	maxRounds := 200
+	rand.Seed(int64(1))
+
+	var gaussians = make([][]float64, len(means))
+
+	for i := 0; i<len(means); i++{
+		var tmpDist = distuv.Normal{Mu:float64(means[i]), Sigma: sigma}
+		tmpvec := make([]float64, numObs)
+		for j := 0; j<numObs; j++{
+			tmpvec[j] = tmpDist.Rand()
+		}
+		gaussians[i] = tmpvec
+	}
+
+	dataset := GenerateMixtureGaussians(responsability, gaussians)
+	PlotDataset("hist.png", dataset)
+
+	run:=0
+	maxRun := 20
+
+	for ;run<maxRun;{
+		rand.Seed(rand.Int63())
+		logs, muZero, muOne, sigmaZero, sigmaOne, piHat:= EMAlgo(dataset, maxRounds)
+
+		fmt.Printf("===================== \n")
+		fmt.Printf("RUN: %v \n", run)
+		fmt.Printf("mu_zero: %v \n", muZero)
+		fmt.Printf("mu_one: %v \n", muOne)
+		fmt.Printf("sigma_zero: %v \n", sigmaZero)
+		fmt.Printf("sigma_one: %v \n", sigmaOne)
+		fmt.Printf("pi_hat: %v \n", piHat)
+		fmt.Printf("last_log: %v \n", logs[len(logs)-1])
+		run++
+	}
+
 }
